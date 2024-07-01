025abdafbdb1ec95e50a6474548fa71ff564f29a
Haruya Okatani <okatani4869@gmail.com>
2024-07-01 23:40:45 +0900

    jsonに予定を保持してカレンダーに表示

---
+++ src/calendar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Calendar } from 'antd';
+import eventsData from './test.json'; // ファイルのインポート
+
+const CalendarComponent = () => {
+  const [events] = useState(eventsData); // useStateを使ってデータを取得
+
+  const dateCellRender = (value) => {
+    const date = value.format('YYYY-MM-DD');
+    const eventInfo = events.find(event => event.date === date);
+
+    if (eventInfo) {
+      return (
+        <ul>
+          {eventInfo.events.map((event, index) => (
+            <li key={index}>{event}</li>
+          ))}
+        </ul>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <Calendar dateCellRender={dateCellRender} />
+  );
+};
+
+export default CalendarComponent;
