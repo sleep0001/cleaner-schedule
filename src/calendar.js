d35f52ac6de972997e8de734790de2f89a81bb62
Haruya Okatani <okatani4869@gmail.com>
2024-08-03 21:53:54 +0900

    #1 テスト環境更新（DynamoDBから取得、交換）

--- src/calendar.js
+++ src/calendar.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
-import { Calendar, Modal, Switch } from 'antd';
-import eventsData from './schedule.json'; // ファイルのインポート
+import React, { useState, useEffect } from 'react';
+import { Calendar, Switch, Button } from 'antd';
+import axios from 'axios';
 import './CalendarComponent.css'; // スタイルシートのインポート
 
 const CalendarComponent = () => {
-  const [events] = useState(eventsData); // useStateを使ってデータを取得
   const [isChangeMode, setIsChangeMode] = useState(false);
   const [selectedDate, setSelectedDate] = useState({});
   const [currentMonth, setCurrentMonth] = useState(null);
 
+  // DynamoDBへのアクセス
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // データ取得中の状態を管理
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://d0ns4u2oaj.execute-api.ap-northeast-1.amazonaws.com/items');
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      } finally {
+        setLoading(false); // データ取得完了後にローディング状態を解除
+      }
+    };
+    fetchData();
+  }, []);
+
   const getClassName = (eventInfo) => {
-    if (!eventInfo || !eventInfo.events) return 'ant-picker-calendar-date-content';
+    if (!eventInfo || !eventInfo.people) return 'ant-picker-calendar-date-content';
 
     const classNames = {
       '野際': 'team1',
@@ -28,7 +46,7 @@ const CalendarComponent = () => {
     }
 
     for (const [key, value] of Object.entries(classNames)) {
-      if (eventInfo.events.includes(key)) return `ant-picker-calendar-date-content ${value}`;
+      if (eventInfo.people.includes(key)) return `ant-picker-calendar-date-content ${value}`;
     }
 
     return 'ant-picker-calendar-date-content holiday';
@@ -36,17 +54,22 @@ const CalendarComponent = () => {
 
   const cellRender = (value) => {
     const date = value.format('YYYY-MM-DD');
-    const eventInfo = events.find(event => event.date === date);
+    const eventInfo = data.find(event => event.date === date);
 
     const className = getClassName(eventInfo);
 
     return (
       <div className={className}>
-        {eventInfo?.events?.map((event, index) => (
-          <div key={index} className="event-item">{event}</div>
+        {/* eventInfo.dataが存在する場合のみmapメソッドを使用 */}
+        {eventInfo && eventInfo.people && eventInfo.people.length > 0 && eventInfo.people.map((person, index) => (
+          <div key={index} className="event-item">{person}</div>
         ))}
       </div>
     );
+  };
+
+  if (loading) {
+    return <div>Loading...</div>; // データ取得中はローディングメッセージを表示
   };
 
   const onSelect = (value) => {
@@ -59,19 +82,19 @@ const CalendarComponent = () => {
     }
 
     const date = value.format('YYYY-MM-DD');
-    const eventInfo = events.find(event => event.date === date);
+    const eventInfo = data.find(event => event.date === date);
 
     // 現在の月と選択された日付の月が一致しない場合は処理をスキップ
     if (currentMonth && value.format('YYYY-MM') !== currentMonth.format('YYYY-MM')) {
       return;
     }
-    
 
-    const newSelected = {...selectedDate};
+
+    const newSelected = { ...selectedDate };
     if (newSelected.hasOwnProperty(eventInfo.date)) {
       delete newSelected[eventInfo.date];
     } else if (Object.keys(newSelected).length < 2) {
-      newSelected[eventInfo.date] = eventInfo.events;
+      newSelected[eventInfo.date] = eventInfo.people;
     } else {
       console.log(newSelected);
       return;
@@ -92,20 +115,52 @@ const CalendarComponent = () => {
       console.log('トグルがオンにされた');
     }
     setIsChangeMode(!isChangeMode);
-
   }
+
+  const handleClick = async () => {
+    const newSelected = { ...selectedDate };
+    // 選択日が2つあるなら交換、そうでないならフラッシュメッセージを残す。（一旦何も起きずにスキップで）
+    if (Object.keys(newSelected).length === 2) {
+      const keys = Object.keys(selectedDate);
+      const firstDate = keys.length > 0 ? selectedDate[keys[0]] : null;
+      const secondDate = keys.length > 1 ? selectedDate[keys[1]] : null;
+      const myInit = [
+        {
+          "date": keys[0],
+          "events": "false",
+          "people": secondDate
+        },
+        {
+          "date": keys[1],
+          "events": "false",
+          "people": firstDate
+        }
+      ]
+      try {
+        const response1 = await axios.put('https://d0ns4u2oaj.execute-api.ap-northeast-1.amazonaws.com/items', myInit[0]);
+        const response2 = await axios.put('https://d0ns4u2oaj.execute-api.ap-northeast-1.amazonaws.com/items', myInit[1]);
+        console.log('Success:', response1, response2);
+        // 成功後にページをリロード
+        window.location.reload();
+      } catch (error) {
+        console.log('Error:', error);
+      };
+    };
+  };
 
   return (
     <>
-      <Switch
-        onClick={toggleSwitch}
-        style={{
-          margin: 16,
-        }}
-      />
-      <Calendar cellRender={cellRender} onSelect={onSelect} onPanelChange={onPanelChange}/>
+        <a>交換モード</a>
+        <Switch
+            onClick={toggleSwitch}
+            style={{
+                margin: 16,
+            }}
+        />
+        <Button type="primary" onClick={handleClick}>CHANGE</Button>
+        <Calendar cellRender={cellRender} onSelect={onSelect} onPanelChange={onPanelChange} />
     </>
-  );
+);
 };
 
 export default CalendarComponent;
