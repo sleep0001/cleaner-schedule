2015903c800020f5f105ff9378c84eb260556800
Haruya Okatani <okatani4869@gmail.com>
2024-08-12 13:22:47 +0900

    #37 ログを取得する機構の追加

--- src/LogsPage.js
+++ src/LogsPage.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './LogsPage.css';
+import axios from 'axios';
 
 const LogsPage = () => {
-    return (
-        <div>
-            ログページ
-        </div>
-    )
+  const [logsData, setLogsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://9uhunbcmd3.execute-api.ap-northeast-1.amazonaws.com/items');
+        setLogsData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      logsData
+    </div>
+  )
 }
 
 export default LogsPage;
