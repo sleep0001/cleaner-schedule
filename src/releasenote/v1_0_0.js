9d8801c738bc2f2c1c57a6c910a1dcf029964546
Haruya Okatani <okatani4869@gmail.com>
2024-09-21 12:48:11 +0900

    #55 1.2.0 リリースノート作成　CSSの整備、過去分のリリースノートのタグを修正

--- src/releasenote/v1_0_0.js
+++ src/releasenote/v1_0_0.js
@@ -5,13 +5,13 @@ const V1_0_0 = () => {
   return (
     <div className='releasenote'>
       <h1>v1.0.0 リリースノート</h1>
-      <hr></hr>
+      <hr />
       <ul>
-        <li><strong>リリース日：</strong> 2024年7月2日</li>
-        <li><strong>リリースバージョン：</strong> v1.0.0</li>
+        <li><strong>リリース日：</strong>2024年7月2日</li>
+        <li><strong>リリースバージョン：</strong>v1.0.0</li>
       </ul>
-      <h1>概要</h1>
-      <hr></hr>
+      <h2>概要</h2>
+      <hr />
       <p>v1.0.0では以下の機能を追加しています。</p>
       <ul>
         <li>当番表（閲覧のみ）</li>
